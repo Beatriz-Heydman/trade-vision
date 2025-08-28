5f63845b92c1d2ffde80da5d93a002e0f7e26fb3
Beatriz Heydman <biaheydman@outlook.com>
2025-08-28 15:39:07 -0300

    feat: create page home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,88 @@
+// Libs
+import { PiPlusBold } from "react-icons/pi";
+import { HiTrendingUp } from "react-icons/hi";
+import { RxQuestionMarkCircled } from "react-icons/rx";
+import { LuChartBar } from "react-icons/lu";
+import { FaTrophy } from "react-icons/fa6";
+
+//Components
+import { Button } from "../../components/button";
+import { Flex } from "../../components/flex";
+import { Typography } from "../../components/typography";
+
+// Styles
+import { StyledHomePage } from "./styles";
+import { Card } from "../../components/card";
+
+export function HomePage() {
+  return (
+    <StyledHomePage>
+      <Flex
+        style={{ width: "100%" }}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Flex
+          style={{ width: "100%" }}
+          direction="column"
+          alignItems="flex-start"
+        >
+          <Typography fontWeight="600" fontSize="1.8rem" color="#000">
+            Visão Geral das Operações
+          </Typography>
+
+          <Typography fontWeight="400" fontSize="1rem" color="#4B5563">
+            Acompanhe seus trades e performance em tempo real
+          </Typography>
+        </Flex>
+
+        <Button
+          onClick={() => {
+            console.log("clicou");
+          }}
+        >
+          <PiPlusBold color="#fff" fontSize="1.2rem" />
+          <Typography fontSize="1rem" fontWeight="400" fontFamily="Poppins">
+            Cadastrar Operação
+          </Typography>
+        </Button>
+      </Flex>
+
+      <Flex
+        style={{ width: "100%" }}
+        justifyContent="space-between"
+        alignItems="flex-start"
+        gap="1rem"
+      >
+        <Card
+          title="Total P&L"
+          value="+R$ 12.450"
+          icon={<HiTrendingUp color="#10b981" size={23} />}
+          color="#DCFCE7"
+          textColor="#10B981"
+        />
+        <Card
+          title="Taxa de Acerto"
+          value="68%"
+          icon={<RxQuestionMarkCircled color="#2563eb" size={23} />}
+          color="#DBEAFE"
+          textColor="#000"
+        />
+        <Card
+          title="Trades Realizados"
+          value="142"
+          icon={<LuChartBar color="#9333ea" size={22} />}
+          color="#F3E8FF"
+          textColor="#000"
+        />
+        <Card
+          title="Maior Gain"
+          value="+R$ 2.890"
+          icon={<FaTrophy color="#f59e0b" size={23} />}
+          color="#FEF9C3"
+          textColor="#10B981"
+        />
+      </Flex>
+    </StyledHomePage>
+  );
+}
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,12 @@
+// Libs
+import styled from "styled-components";
+
+export const StyledHomePage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
