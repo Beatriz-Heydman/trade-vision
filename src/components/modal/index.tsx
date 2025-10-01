fa1115aefe178679a8c9f7b98c7d37f98de59368
Beatriz Heydman <biaheydman@outlook.com>
2025-10-01 15:13:03 -0300

    fix: update onClick handler in Modal component to close on overlay click

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -5,8 +5,16 @@ import { StyledModal } from "./styles";
 import type { ModalProps } from "./types";
 
 export function Modal({ isOpen, onClose, children }: ModalProps) {
+  const overlayClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <StyledModal data-is-open={isOpen} onClick={onClose}>
+    <StyledModal data-is-open={isOpen} onClick={overlayClick}>
       <div className="modal-content">{children}</div>
     </StyledModal>
   );
