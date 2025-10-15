c568e00dd4d5098758a92b090fbb8acd14c19580
Beatriz Heydman <biaheydman@outlook.com>
2025-10-15 16:51:38 -0300

    feat: add keyboard escape functionality to modal component

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,5 @@
 // Styles
+import { useEffect } from "react";
 import { StyledModal } from "./styles";
 
 // Types
@@ -12,6 +13,26 @@ export function Modal({ isOpen, onClose, children }: ModalProps) {
       onClose();
     }
   };
+
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+        console.log("Modal fechado", handleEsc);
+      }
+    };
+
+    // Para que o event listener só seja adicionado quando o modal estiver aberto.
+    if (isOpen) {
+      window.addEventListener("keydown", handleEsc);
+      console.log("Modal aberto", handleEsc);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+      console.log("Removendo listener", handleEsc);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <StyledModal data-is-open={isOpen} onClick={overlayClick}>
